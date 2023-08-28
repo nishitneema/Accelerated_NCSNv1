888ae0d18214ebc83a8b2d9b79af90547d87bad8
Nishit Neema <67251847+nishitneema@users.noreply.github.com>
2023-08-29 00:17:32 +0530

    init

---
+++ script.sh
@@ -0,0 +1,6 @@
+# change cuda device in fid_score.py
+#python main.py --T 10 --doc cifar10 --find_fid -o images_momentum/cifar10/cal_fid
+python main.py --T 20 --doc cifar10 --find_fid -o images_momentum/cifar10/cal_fid
+python main.py --T 30 --doc cifar10 --find_fid -o images_momentum/cifar10/cal_fid
+python main.py --T 40 --doc cifar10 --find_fid -o images_momentum/cifar10/cal_fid
+python main.py --T 50 --doc cifar10 --find_fid -o images_momentum/cifar10/cal_fid
